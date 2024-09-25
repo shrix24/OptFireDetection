3747afb75b8f6c95cb8878d0e0ab8d4adbbec58a
shrix24 <138632320+shrix24@users.noreply.github.com>
2024-09-25 21:05:12 +0100

    Add files via upload

---
+++ include/FeatureExtraction.hpp
@@ -0,0 +1,11 @@
+#ifndef FEATUREEXTRACTION_HPP
+#define FEATUREEXTRACTION_HPP
+#include <opencv4/opencv2/imgproc.hpp>
+
+#define Y_CR_CB
+#define HSV
+#define LAB
+#define HLS
+
+class Window_Features;
+#endif

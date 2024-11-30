a9dadd261afd8309a1b646f43c2c5b554f9c1aab
shrix24 <138632320+shrix24@users.noreply.github.com>
2024-11-30 14:48:13 +0000

    Update to coordinate handling in YOLO module

--- JetsonCode/Yolo_main.py
+++ JetsonCode/Yolo_main.py
@@ -3,6 +3,7 @@ import cv2
 from ultralytics import YOLO
 import logging
 from signalrcore.hub_connection_builder import HubConnectionBuilder
+from GlobalVars import global_vars
 
 
 class YOLO_Detect:
@@ -27,22 +28,22 @@ class YOLO_Detect:
         self.handler = logging.StreamHandler()
         logging.disable() # disable it by default... it's verbose!
 
-        print("Building Connection to Platform Control")
-        self.hub_connection = HubConnectionBuilder().with_url(self.PLATFORM_CONNECTION_URL + "/autopilotHub").configure_logging(logging.DEBUG, socket_trace=True, handler=self.handler).build()
+        # print("Building Connection to Platform Control")
+        global_vars.hub_connection = HubConnectionBuilder().with_url(self.PLATFORM_CONNECTION_URL + "/autopilotHub").configure_logging(logging.DEBUG, socket_trace=True, handler=self.handler).build()
 
-        print("Signing up to open, close and error events")
+        # print("Signing up to open, close and error events")
         # sign up to start stop and error events
-        self.hub_connection.on_open(self.onOpen)
-        self.hub_connection.on_close(self.onClose)
-        self.hub_connection.on_error(self.onError)
+        global_vars.hub_connection.on_open(self.onOpen)
+        global_vars.hub_connection.on_close(self.onClose)
+        global_vars.hub_connection.on_error(self.onError)
 
-        print("Signing up to location and attitude broadcasting")
-        self.hub_connection.on("BroadcastLocation", self.handleLocation)
-        self.hub_connection.on("BroadcastAttitude", self.handleAttitude)
+        # print("Signing up to location and attitude broadcasting")
+        global_vars.hub_connection.on("BroadcastLocation", self.handleLocation)
+        global_vars.hub_connection.on("BroadcastAttitude", self.handleAttitude)
 
-        print("Starting Connection")
+        # print("Starting Connection")
         # start connection
-        self.hub_connection.start()
+        global_vars.hub_connection.start()
 
     def handleHeartbeat(self, data):
         print(data[0])
@@ -53,18 +54,20 @@ class YOLO_Detect:
 
         # location is sent as a dictionary and can be accessed as shown (same system for other variables)
         # example assignment to variable inside location structure
-        altitudeM_AMSL = data['data']['altM']
-        Latitude = data['data']['latDeg']
-        Longitude = data['data']['lngDeg']
 
-        self.gps_position = np.array[Latitude, Longitude, altitudeM_AMSL]
+        altitudeM_AMSL = data[0]['data']['altM']
+        Latitude = data[0]['data']['latDeg']
+        Longitude = data[0]['data']['lngDeg']
+
+        self.gps_position = np.array([Latitude, Longitude, altitudeM_AMSL])
         # be aware that in python if you want to assign a variable outside the scope of the function you must declare it as 'global'
     
     def handleAttitude(self, data):
         # print(data)
-        roll = data['data']['rollDeg']
-        yaw = data['data']['yawDeg']
-        pitch = data['data']['pitchDeg']
+        roll = data[0]['data']['rollDeg']
+        yaw = data[0]['data']['yawDeg']
+        pitch = data[0]['data']['pitchDeg']
+
         self.uav_attitude = np.array([roll, yaw, pitch])
         # Attitude is in degrees
 
@@ -79,33 +82,56 @@ class YOLO_Detect:
         print("Error | %s" % data.error)
 
     def detect(self, raw_frame):
-        # self.cap = cv2.VideoCapture(self.video_path)
-        # self.frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        # self.frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        # self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
-
-
-        # self.output_video = cv2.VideoWriter('output_video.mp4',
-        #                         cv2.VideoWriter_fourcc(*'mp4v'),
-        #                         self.fps,
-        #                         (self.frame_width, self.frame_height))
         self.frame_count = 0
-        frame = cv2.resize(frame, (640, 480), cv2.INTER_AREA)
-        # Run inference on the current frame using the custom model
-        results = self.model.predict(frame)
 
-        # Visualize the predictions directly on the frame
-        annotated_frame = results[0].plot()  # Annotated frame with bounding boxes
+        try:
+            if self.frame_count%30 == 0 or self.frame_count == 0:
+                x_coords = []
+                y_coords = []
 
-        # Write the annotated frame to the output video
-        # output_video.write(annotated_frame)
-        
-        # Display the frame (optional)
-        # cv2.imshow('YOLO Detection', annotated_frame)
-        self.frame_count += 1
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+                frame = cv2.resize(frame, (640, 480), cv2.INTER_AREA)
+                og_image = frame.copy()
+                # Run inference on the current frame using the custom model
+                results = self.model.predict(frame)
 
+                # Visualize the predictions directly on the frame
+                annotated_frame = results[0].plot()  # Annotated frame with bounding boxes
+
+                x_coords = []
+                y_coords = []
+
+                for result in results:
+                    boxes = result.boxes
+
+                    if not boxes:
+                        global_vars.img_coords = np.array([0, 0])
+                        break
+
+                    for box in boxes:
+                        x1, y1, x2, y2 = map(int, box.xyxy[0])
+                        cls = box.cls[0]
+
+                        if cls == 1:
+                            x_coords.append(x1)
+                            x_coords.append(x2)
+                            y_coords.append(y1)
+                            y_coords.append(y2)
+
+                x_mean = np.mean(np.array(x_coords))
+                y_mean = np.mean(np.array(y_coords))
+                global_vars.img_coords = np.array([x_mean, y_mean])
+
+                global_vars.image_to_transmit = frame
+                global_vars.og_image_to_transmit = annotated_frame
+
+                if self.gps_position.size > 0 and self.uav_attitude.size > 0:
+                    global_vars.gps_position = self.gps_position
+                    global_vars.uav_attitude = self.uav_attitude
+
+            self.frame_count += 1
+
+        except Exception as error:
+            print(error)
 
 if __name__ == "__main__":
     # vision_module = YOLO_Detect()
